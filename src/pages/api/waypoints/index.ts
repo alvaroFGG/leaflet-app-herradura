62c255b0df1b439354ee358d702b9f7a539a436a
Álvaro Fuentenebro <alvarofuentenebrogomez@gmail.com>
2023-01-31 17:23:04 +0100

    fix: added forgotten put method

--- src/pages/api/waypoints/index.ts
+++ src/pages/api/waypoints/index.ts
@@ -19,14 +19,32 @@ const createWaypoint = async (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json(waypoint);
 };
 
+const updateWaypoint = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { id, name, description, location, type } = req.body;
+  const waypoint = await WaypointModel.findByIdAndUpdate(id, {
+    name,
+    description,
+    location,
+    type,
+  });
+  res.status(200).json(waypoint);
+};
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   ProviderDatabase.init();
 
-  if (req.method === "GET") {
-    await getWaypoints(res);
-  }
-
-  if (req.method === "POST") {
-    await createWaypoint(req, res);
+  switch (req.method) {
+    case "GET":
+      await getWaypoints(res);
+      break;
+    case "POST":
+      await createWaypoint(req, res);
+      break;
+    case "PUT":
+      await updateWaypoint(req, res);
+      break;
+    default:
+      res.status(405).end();
+      break;
   }
 };
