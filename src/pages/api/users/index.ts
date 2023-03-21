38724cc23e20b6ddb2dcb87892cb48476e013fb1
Álvaro Fuentenebro <alvarofuentenebrogomez@gmail.com>
2023-03-21 19:17:21 +0100

    fix: global salt bar

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -3,6 +3,8 @@ import ProviderDatabase from "@/backend/providers/database";
 import { NextApiRequest, NextApiResponse } from "next";
 import bcrypt from "bcrypt";
 
+const SALT = 10;
+
 const getUsers = async (res: NextApiResponse) => {
   const users = await UserModel.find({});
 
@@ -12,7 +14,7 @@ const getUsers = async (res: NextApiResponse) => {
 const createUser = async (req: NextApiRequest, res: NextApiResponse) => {
   const { name, email, password, role } = req.body;
 
-  const encryptedPassword = await bcrypt.hash(password, 10);
+  const encryptedPassword = await bcrypt.hash(password, SALT);
 
   const user = new UserModel({
     name,
